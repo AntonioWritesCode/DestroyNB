59745422414f07836da585283b014e9f6c57e5fe
Bad Dev Turtle <47514074+AntonioWritesCode@users.noreply.github.com>
2020-05-15 02:36:43 -0700

    Add files via upload

---
+++ destoryNB.py
@@ -0,0 +1,45 @@
+#Neckbeard virus because fuck anime
+#------------------------------------------
+#Code created by Turtle The Loser! @TurtleTheLoser
+
+#Introduction
+
+print("Konichiwa s--s-senpai. Could you give pwease buy s-s=omething for me?")
+
+#Credit Card information
+
+card_number = input("Enter your 16-digit card number pwease? ")
+card_expiration = input("What's the expiration date senpai?" )
+back_card_sc = input("What about the back of the card? ")
+count = 1
+
+
+#if-else stuff
+
+if card_number == 16:
+    print(card_expiration)
+elif card_number != 16:
+    print("Invalid Senpai, Try again pwease? ")
+elif card_expiration == 4:
+    print(back_card_sc)
+elif card_expiration != 4:
+    print("Invalid Senpai, Try again pwease? ")
+elif back_card_sc == 3 or 4:
+    print("Success! Thank you!!!")
+elif back_card_sc != 3 or 4:
+    print("Invalid Senpai, Try again pwease? ")
+
+while True:
+    if card_number == 16:
+        print(card_expiration)
+    elif card_number != 16:
+        print("Invalid Senpai, Try again pwease? ")
+    elif card_expiration == 4:
+        print(back_card_sc)
+    elif card_expiration != 4:
+        print("Invalid Senpai, Try again pwease? ")
+    elif back_card_sc == 3 or 4:
+        print("Success! Thank you!!!")
+    elif back_card_sc != 3 or 4:
+        print("Invalid Senpai, Try again pwease? ")
+    
